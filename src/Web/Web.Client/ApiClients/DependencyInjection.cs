70f0b5ef13517e084f3de58016cdc0495acf0371
John Doe <annghdev@gmail.com>
2025-11-30 00:20:57 +0700

    Update: simplify services

---
+++ src/Web/Web.Client/ApiClients/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.Client.ApiClients;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiClients(this IServiceCollection services, IConfiguration configuration)
+    {
+        builder.Services.AddScoped(sp => new HttpClient
+        {
+            BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+        });
+        return services;
+    }
+}
---
+++ src/Web/Web.Client/ApiClients/TestApiClient.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Client.ApiClients;
+
+public class TestApiClient(HttpClient httpClient)
+{
+    public async Task<string> GetData()
+    {
+        var response = await httpClient.GetStringAsync("");
+        return response ?? "error!";
+    }
+}
